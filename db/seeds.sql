3a89f93017a0d38a4405c6fd5e60a2a1d2707f53
m3llington <mellingtonucf@knights.ucf.edu>
2019-09-17 23:52:31 -0400

    added burger values into seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Quesadilla Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Peppercorn Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Jalapeno Burger");
