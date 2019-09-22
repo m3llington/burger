3151b56971c1244559ba1414a3c3fe393d55d0ac
m3llington <mellingtonucf@knights.ucf.edu>
2019-09-22 15:42:28 -0400

    resolved issue with burgers that were choosen not properly displaying

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,9 @@
-INSERT INTO burgers (burger_name) VALUES ("Quesadilla Burger");
+INSERT INTO burgers (burger_name) VALUES ('Quesadilla Burger');
 
-INSERT INTO burgers (burger_name) VALUES ("Peppercorn Burger");
+INSERT INTO burgers (burger_name) VALUES ('Peppercorn Burger');
 
-INSERT INTO burgers (burger_name) VALUES ("Jalapeno Burger");
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Burger');
+
+INSERT INTO burgers (burger_name, eaten) VALUES ('Blooming Burger', true);
+
+INSERT INTO burgers (burger_name, eaten) VALUES ('Bacon Cheeseburger', true);
